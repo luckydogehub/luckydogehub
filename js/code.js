82f29d174bfc17de70c971118443e0226a8c9a99
luckydogehub <luckydoge0@gmail.com>
2021-06-25 02:48:36 +0300

    populate roadmap

--- js/code.js
+++ js/code.js
@@ -8,7 +8,7 @@ particlesJS.load('particles-js', 'js/particles2.json', function() {
 function handleTickInit(tick) {
     let dt = new Date();
     //let to = '2021-' + (dt.getUTCMonth() + 1) + '-' + (dt.getUTCDate() + 1);
-    let to = '2021-06-25';
+    let to = '2021-06-28';
     Tick.count.down(to).onupdate = function(value) {
         tick.value = value;
     };
