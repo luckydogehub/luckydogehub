4997c9e3a895cf1306a674b51429d1c804ecc947
luckydogehub <luckydoge0@gmail.com>
2021-06-25 23:33:11 +0300

    new colors

--- js/code.js
+++ js/code.js
@@ -30,6 +30,7 @@ function scrollDisplay() {
 }
 
 function scrollTop() {
+  location.hash = '';
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
